dc648bfc25756c18bdb5cd9114a50b5005bf0a39
immorrtalz <61843974+immorrtalz@users.noreply.github.com>
2026-06-22 04:44:25 +0500

    forgot to remove from prev commit

--- src/main/java/com/immorrtalz/beautifulcarpets/BeautifulCarpets.java
+++ src/main/java/com/immorrtalz/beautifulcarpets/BeautifulCarpets.java
@@ -26,27 +26,6 @@ public class BeautifulCarpets
 		ModItems.ITEMS.register(modEventBus);
 		ModCreativeTabs.TABS.register(modEventBus);
 
-		modEventBus.addListener(BeautifulCarpets::onGatherData);
-
 		LOGGER.info("BeautifulCarpets client initialized.");
-	}
-
-	public static void onGatherData(GatherDataEvent event)
-	{
-		event.getGenerator().addProvider(
-			event.includeServer(),
-			output -> new ModBlockLootProvider(
-				output,
-				// A set of required table resource locations. These are later verified to be present.
-				// It is generally not recommended for mods to validate existence,
-				// therefore we pass in an empty set.
-				Set.of(),
-				// A list of sub provider entries. See below for what values to use here.
-				List.of(new SubProviderEntry(
-					// A reference to the sub provider's constructor.
-					// This is a Function<HolderLookup.Provider, ? extends LootTableSubProvider>.
-					MyLootTableSubProvider::new,
-					// An associated loot context set. If you're unsure what to use, use empty.
-					LootContextParamSets.EMPTY))));
 	}
 }
